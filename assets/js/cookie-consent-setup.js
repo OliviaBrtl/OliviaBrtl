5b295c5a4f501e8879dabd3392a93a1f6e9dbc72
OliviaBrtl <160524154+OliviaBrtl@users.noreply.github.com>
2026-03-23 13:48:58 +0000

    Deploying to gh-pages from @ OliviaBrtl/OliviaBrtl@0d34f6931422572a3fd0cd28569b56bc999737db 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://OliviaBrtl.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://OliviaBrtl.github.io/OliviaBrtl/#contact">contact us</a>.'
               }
             ]
           }
